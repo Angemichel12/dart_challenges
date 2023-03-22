fb0cd6626a30d1813fd3a64dc75ffdae12cac075
Angemichel12 <95592087+Angemichel12@users.noreply.github.com>
2023-03-22 18:12:50 +0200

    solve bugs from challenge2

--- challenge2.dart
+++ challenge2.dart
@@ -5,13 +5,10 @@ void main() {
 }
 
 void check_latter() {
-  String user_word = stdin.readLineSync()!;
-  if (user_word.length > 0) {
-    bool isValid =
-        user_word.substring(0, 1) == user_word.substring(user_word.length - 1);
-    String result = isValid ? "Equal" : "Not Equall";
-    print(result);
-  } else {
-    print("Empty String");
-  }
+  print('Enter word');
+  String? UserInput = stdin.readLineSync()!;
+  bool isValid =
+      UserInput.substring(0, 1) == UserInput.substring(UserInput.length - 1);
+  String result = isValid ? "Equal" : "Not Equal";
+  print(result);
 }
